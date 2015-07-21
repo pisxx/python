9faab4b3449662ff56a4973da4b662b895b9586e
Pisxx <piotrek.slawek@gmail.com>
2015-07-21 09:48:15 +0200

    ex3.py

--- ex/ex3.py
+++ ex/ex3.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python
-
+#print
 print "I will now count my chickens"
+#dodaje i dzieli
+print "Hens", 25 + 30 / 6
+#odemuje mnozy i modulu(reszta z dzielenia)
+print "Roosters", 100 - 25 * 3 % 4
+#pisze
+print "Now I will count the eggs:"
 
-print "
+print 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6
 
+print "Is this true that 3 + 2 < 5 - 7?"
+#wyswietli boolean True or False z dzialania
+print 3 + 2 < 5 - 7 
+
+print "What is 3 + 2?", 3 + 2
+
+print "What is 5 -7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+#wyswietla True or False z dzialania
+print "Is it greater?", 5 > 2
+print "Is it greater or equal?", 5 >= 2
+print "Is it less or equal?", 5 <= -2
