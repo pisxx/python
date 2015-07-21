9ccced6635d152c6bb416d4a72def25a724c2b82
Pisxx <piotrek.slawek@gmail.com>
2015-07-21 09:52:51 +0200

    py.sh

--- py.sh
+++ py.sh
@@ -15,8 +15,7 @@ then
 #	vim $1.py
 elif [ ! -f $1.py -o ! -f $1 ]
 then
-	touch $1.py
-	echo -e '#!/usr/bin/python\n' >> $1.py
+	echo -e '#!/usr/bin/python\n' > $1.py
 	chmod +x $1.py
 	vim $1.py
 fi
