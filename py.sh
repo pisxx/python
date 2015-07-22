c1456ea20e4ec84e104fca1452c0a4a2dd25fc95
pisxx <piotrek.slawek@gmail.com>
2015-07-22 14:29:54 +0200

    py.sh

--- py.sh
+++ py.sh
@@ -16,7 +16,7 @@ then
 elif [ ! -f $1.py -o ! -f $1 ]
 then
 	#echo -e '#!/usr/bin/python\n' > $1.py
-	echo -e '/usr/local/bin/python2.7\n' > $1.py
+	echo -e '#!/usr/local/bin/python2.7\n' > $1.py
 	chmod +x $1.py
 	vim $1.py
 fi
