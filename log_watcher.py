a2ff9ece98b4aa0e591824b540e43cba5a8c43af
pisxx <pisxx@users.noreply.github.com>
2016-03-21 00:40:24 +0100

    Update log_watcher.py

--- log_watcher.py
+++ log_watcher.py
@@ -3,10 +3,11 @@ import os
 import sys
 import pdb
 
-file_name = sys.argv[1]
 #file_name = 'plik'
+file_name = sys.argv[1]
 file_size = os.path.getsize(file_name)
 
+pattern = sys.argv[2]
 print "Wielkosc pliku to -> %s " % file_size
 
 file_pos = open('pos', 'r')
@@ -16,11 +17,16 @@ last_pos = int(file_pos.readline().strip("'").rstrip('\n'))
 print "Poprzednia ostatnia linia  w pliku to -> %s " % last_pos
 if file_size < last_pos:
     last_pos = 0
+if last_pos == file_size:
+    print "Nie ma nic nowego"
+    sys.exit(1)
 
 with open(file_name, 'rb') as plik:
     try:
         plik.seek(last_pos)
         for line in iter(plik.readline, ''):
+            if pattern in line:
+                print "------------"
             print "to jest linia -> %s " % line.strip('\n')
             pos = plik.tell()
             print "to jest polozenie w pliku -> %s " % pos
